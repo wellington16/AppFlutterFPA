239e415317fb2c076744fef7386533ca0465d1f4
Wellington Luiz Antonio <wla@NEUROTECH.INTRANET>
2020-09-07 21:50:19 -0300

    Adicionando a tela de splash e de alerta

---
+++ lib/pages/alert_page.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+
+import 'contact_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: SizedBox(
+            child: Text(
+              "Caí Aqui",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Color(0XFF23af23),
+              ),
+            ),
+          ),
+        ),
+        leading: Container(
+          height: 40,
+          width: 40,
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: AssetImage("assets/profile-icon.png"),
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          Container(
+            width: 60,
+            child: FlatButton(
+              child: Icon(
+                Icons.more_vert,
+                color: Color(0xFFBABABA),
+              ),
+              onPressed: () => {},
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        color: Color(0xFFF2F3F6),
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              child: Center(
+                child: Text(
+                  "Clique em monitorar para começar",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              child: Center(
+                child: Text(
+                  "O Caí Aqui detecta quedas acidentais e envia um sinal de emergência para os números cadastrados",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0XFF23af23),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: Image.asset("assets/logo.png"),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+                // color: Colors.green,
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color(0XFF23af23),
+                    Color(0XFF672c81),
+                  ],
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Monitorar",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Container(
+                        child: SizedBox(
+                          child: Image.asset("assets/logo.png"),
+                          height: 28,
+                          width: 28,
+                        ),
+                      )
+                    ],
+                  ),
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    // builder: (context) => LoginPage(),
+                    //   ),
+                    // );
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+                // color: Colors.green,
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color(0XFF87cefa),
+                    Color(0XFF672c81),
+                  ],
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Configurações",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Container(
+                        child: SizedBox(
+                          child: Image.asset("assets/logo.png"),
+                          height: 28,
+                          width: 28,
+                        ),
+                      )
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ContatosPage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  return new WillPopScope(
+    child: new Scaffold(
+      appBar: new AppBar(
+          // title: new Text('Page 2'),
+          ),
+      body: new Center(
+          // child: new Text('PAGE 2'),
+          ),
+    ),
+    onWillPop: () async {
+      return false;
+    },
+  );
+}
+
+Future<T> pushPage<T>(BuildContext context, Widget page) {
+  return Navigator.of(context)
+      .push<T>(MaterialPageRoute(builder: (context) => page));
+}
